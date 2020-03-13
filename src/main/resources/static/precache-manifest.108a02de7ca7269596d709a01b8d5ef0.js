fca7ab064558e1d51b015b97ba70f59fb6f52148
Chris <cldiaz06@gmail.com>
2020-03-13 16:00:59 -0500

    Added react components

---
+++ src/main/resources/static/precache-manifest.108a02de7ca7269596d709a01b8d5ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9138e990046106d2c4c505ee218d2bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76bb4744790c768bcb1a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a175100dffce79a8799d",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "76bb4744790c768bcb1a",
+    "url": "/static/js/2.d04aa948.chunk.js"
+  },
+  {
+    "revision": "a175100dffce79a8799d",
+    "url": "/static/js/main.36eb3ac6.chunk.js"
+  },
+  {
+    "revision": "01092b790686342d17d0",
+    "url": "/static/js/runtime-main.6a56ce4f.js"
+  }
+]);
